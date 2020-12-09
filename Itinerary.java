c01d9fe6c024b5150f1bd1b669cd4115a31a4ef0
Armin <73317759+Armin-Irvije@users.noreply.github.com>
2020-12-09 13:24:07 -0800

    Add files via upload

---
+++ Itinerary.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Itinerary {
+    protected String event;
+    protected String location;
+    protected String description;
+
+   public Itinerary(String userEvent, String meetinglocation, String eventDescription){
+        event = userEvent;
+        location = meetinglocation;
+        description = eventDescription;
+    }
+    public void printItinerary(){
+        System.out.println();
+        System.out.println("Your event: " + event);
+        System.out.println("Meeting location: " + location);
+        System.out.println("Description of meeting: "+ description);
+    }
+
+}
